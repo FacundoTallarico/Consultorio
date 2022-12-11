9d00449539dc35f57ffbc073fe9502b3c2056855
FacundoTallarico <facundotalla@gmail.com>
2022-12-10 23:50:54 -0300

    actualiza

---
+++ assets/estilos/main.js
@@ -0,0 +1,18 @@
+const section = document.querySelector('header > nav');
+let prevY = window.scrollY;
+
+// console.log(div.innerHTML)
+window.addEventListener('scroll', function(){
+    // if(prevY > window.scrollY) {
+    //     section.classList.remove('off')
+    // } else {
+    //     section.classList.add('off')
+    // }
+
+    if (window.scrollY > 550) {
+        section.classList.add('solid')
+    } else{
+        section.classList.remove('solid')
+    }
+    prevY = window.scrollY
+})

7ec1bc1693fb73072a8580be18b6d76886f9c303
FacundoTallarico <facundotalla@gmail.com>
2023-01-19 06:26:01 -0300

    slide2

--- assets/estilos/main.js
+++ assets/estilos/main.js
@@ -1,4 +1,5 @@
 const section = document.querySelector('header > nav');
+const navbarBrand = document.querySelector('header > nav > div > a');
 let prevY = window.scrollY;
 
 // console.log(div.innerHTML)
@@ -10,9 +11,11 @@ window.addEventListener('scroll', function(){
     }
 
     if (window.scrollY > 550) {
-        section.classList.add('solid')
-    } else{
-        section.classList.remove('solid')
+        section.classList.add('solid'),
+        navbarBrand.classList.remove('text-light')
+      } else{
+        section.classList.remove('solid'),
+        navbarBrand.classList.add('text-light')
     }
     prevY = window.scrollY
 })
